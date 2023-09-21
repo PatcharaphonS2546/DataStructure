43a47dd01e38ba4650a3cbeb569c6b79fce8e54d
Patcharaphon Samakun <Patcharaphon Samakun@MSI>
2023-09-21 14:43:18 +0700

    Update Week3

---
+++ Collections/ArrayList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class ArrayList : List
+    {
+        public void add(int index, object e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void add(object e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool contains(object e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object get(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int index(object e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void remove(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void remove(object e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void set(int index, object e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int size()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
